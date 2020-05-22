12cd860e6cef5cec817c010282fd3d38af9d1e6a
Justin Magaram <justin@magaram.com>
2020-05-22 11:27:55 -0700

    Simple ItemListItem component

---
+++ Client/FSharpOptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.FSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Client
+{
+    public static class FSharpOptionExtensions
+    {
+        public static bool IsSome<T>(this FSharpOption<T> o) => FSharpOption<T>.get_IsSome(o);
+
+        public static bool IsNone<T>(this FSharpOption<T> o) => FSharpOption<T>.get_IsNone(o);
+
+        public static IEnumerable<T> AsEnumerable<T>(this FSharpOption<T> o) =>
+            o.IsSome() ? Enumerable.Repeat(o.Value, 1) : Enumerable.Empty<T>();
+
+        public static T GetOr<T>(this FSharpOption<T> o, T ifNone) =>
+            o.IsSome() ? o.Value : ifNone;
+    }
+}
