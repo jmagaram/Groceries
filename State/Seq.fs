7267fb15a3689575625b1db87f9a3f4d3025c4fd
Justin Magaram <justin@magaram.com>
2020-06-11 11:02:47 -0700

    add chunk for aggregating consecutive items in a sequence

--- State/Seq.fs
+++ State/Seq.fs
@@ -17,11 +17,74 @@ let zeroOrOne s =
     | [x] -> Some x
     | _ -> failwith "Too many items in the sequence. Expected zero or one."
 
+let chunk create add source =
+    seq {
+        let mutable chunk = None
+        for i in source do
+            let (chunk', isComplete) = 
+                chunk
+                |> Option.map (fun c -> 
+                        match c |> add i with
+                        | None -> (i |> create, true)
+                        | Some c -> (c, false))
+                |> Option.defaultWith(fun () -> (i |> create, false))
+            if isComplete then
+                yield chunk
+            chunk <- Some chunk'
+        yield chunk }
+    |> Seq.choose id
+
 module Tests = 
 
     open System
     open Xunit
     open FsUnit
+
+    let chunkIntoListsOfSameParity = 
+        let createList i = [i]
+        let consIfSameParity i list =
+            let isListEven = list |> List.tryHead |> Option.map (fun i -> i % 2 = 0) |> Option.defaultValue true
+            let isItemEven = i % 2 = 0
+            if isListEven = isItemEven 
+            then Some (i :: list)
+            else None        
+        chunk createList consIfSameParity
+
+    [<Fact>]
+    let ``chunk - when sequence has many items and many resultant chunks`` () =
+        let result = 
+            [1;3;5;2;4;6;7;9;11]
+            |> chunkIntoListsOfSameParity
+            |> Seq.toList
+        result 
+        |> should equivalent [ [5;3;1]; [6;4;2]; [11;9;7]]
+
+    [<Fact>]
+    let ``chunk - when sequence has many items and just one resultant chunk`` () =
+        let result = 
+            [1;3;5;7;9]
+            |> chunkIntoListsOfSameParity
+            |> Seq.toList
+        result 
+        |> should equivalent [ [9;7;5;3;1]]
+
+    [<Fact>]
+    let ``chunk - when sequence has nothing in it`` () =
+        let result = 
+            []
+            |> chunkIntoListsOfSameParity
+        result 
+        |> Seq.isEmpty
+        |> should equal true
+
+    [<Fact>]
+    let ``chunk - when sequence has exactly one item`` () =
+        let result = 
+            [1]
+            |> chunkIntoListsOfSameParity
+            |> Seq.toList
+        result 
+        |> should equivalent [ [1] ]
 
     [<Fact>]
     let ``zero or one - when empty expect none`` () =
