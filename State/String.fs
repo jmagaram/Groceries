52f891f25d7e609ea295a079f41c4819e2695eea
Justin Magaram <justin@magaram.com>
2020-07-08 11:36:59 -0700

    repeat string function

--- State/String.fs
+++ State/String.fs
@@ -1,6 +1,8 @@
 ﻿[<AutoOpen>]
 module String
 
+open System
+
 let isNullOrWhiteSpace s = System.String.IsNullOrWhiteSpace(s)
 
 let trim (s:string) = 
@@ -20,11 +22,50 @@ let tryParseGuid = tryParseWith System.Guid.TryParse
 
 let tryParseTimeSpan = tryParseWith System.TimeSpan.TryParse
 
+let repeat n s = 
+    if n < 0
+        then raise (ArgumentOutOfRangeException("n","The repeat count must be greater than or equal to zero."))
+    elif s = null
+        then raise (ArgumentNullException("s"))
+    else
+        String.Join("", Seq.replicate (n+1) s)
+
 module Tests = 
 
     open System
     open Xunit
     open FsUnit
+    open FsCheck
+    open FsCheck.Xunit
+
+    type ZeroOrPositive =
+        static member Integer() = 
+            Arb.Default.Int32()
+            |> Arb.mapFilter abs (fun t -> t >= 0)
+
+    type NotNullString =
+        static member String() =
+            Arb.Default.String()
+            |> Arb.filter (fun s -> s <> null)
+
+    [<Fact>]
+    let ``repeat when n < 0 should throw`` () =
+        (fun() -> repeat -1 "aaa" |> ignore)
+        |> should throw typeof<ArgumentOutOfRangeException>
+
+    [<Fact>]
+    let ``repeat when passed null should throw`` () =
+        (fun() -> repeat 3 null |> ignore)
+        |> should throw typeof<ArgumentNullException>
+
+    [<Property(Arbitrary=[| typeof<ZeroOrPositive>; typeof<NotNullString> |])>]
+    let ``repeat n times is repeat n-1 plus original, but if n = 0 then original`` (s:string) (n:int) = 
+        let actual = s |> repeat n
+        let expected = 
+            match n with
+            | 0 -> s
+            | x -> sprintf("%s%s") (s |> repeat (x-1)) s
+        actual = expected
 
     [<Theory>]
     [<InlineData("1", 1)>]
