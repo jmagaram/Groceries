7ee28b976a194a9869e75016b78e829c5ce506c2
Justin Magaram <justin@magaram.com>
2020-08-28 15:16:40 -0700

    add memoize

--- State/Utilities.fs
+++ State/Utilities.fs
@@ -4,3 +4,14 @@ module Utilities
 let newGuid () = System.Guid.NewGuid()
 
 let nowUtc : DomainTypes.NowUtc = fun () -> System.DateTime.UtcNow
+
+let memoize f = 
+    let mutable cache = Map.empty
+    let f' x =
+        match cache |> Map.tryFind x with
+        | None ->
+            let result = f x
+            cache <- cache |> Map.add x result
+            result
+        | Some result -> result
+    f'
