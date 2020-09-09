3d15f40f633600efaa5c80681b535306d4368ca1
Justin Magaram <justin@magaram.com>
2020-09-09 10:58:48 -0700

    seq tests

--- Models/Utilities.fs
+++ Models/Utilities.fs
@@ -4,19 +4,14 @@ open System
 [<AutoOpen>]
 module Seq =
 
-    let takeAtMost n s =
-        s
-        |> Seq.mapi (fun index item -> if index < n then Some item else None)
-        |> Seq.takeWhile (fun i -> i.IsSome)
-        |> Seq.choose id
-
-    let zeroOrOne s =
-        let result = s |> takeAtMost 2 |> Seq.toList
-
-        match result with
-        | [] -> None
-        | [ x ] -> Some x
-        | _ -> failwith "Too many items in the sequence. Expected zero or one."
+    let takeAtMost n source =
+        if n < 0 then
+            invalidArg "n" "The input must be non-negative."
+        else
+            source
+            |> Seq.mapi (fun index item -> if index < n then Some item else None)
+            |> Seq.takeWhile (fun i -> i.IsSome)
+            |> Seq.choose id
 
 [<AutoOpen>]
 module Map =
@@ -28,18 +23,4 @@ module String =
 
     let trim (s: String) = s.Trim()
 
-    let isNullOrWhiteSpace s = System.String.IsNullOrWhiteSpace(s)
-
-    let tryParseWith (tryParseFunc: string -> bool * _) =
-        tryParseFunc
-        >> function
-        | true, v -> Some v
-        | false, _ -> None
-
-    let tryParseDate = tryParseWith System.DateTime.TryParse
-
-    let tryParseInt = tryParseWith System.Int32.TryParse
-
-    let tryParseGuid = tryParseWith System.Guid.TryParse
-
-    let tryParseTimeSpan = tryParseWith System.TimeSpan.TryParse
+    let isNullOrWhiteSpace s = String.IsNullOrWhiteSpace(s)
---
+++ ModelsTest/UtilitiesTests.fs
@@ -0,0 +1,34 @@
+﻿namespace ModelsTest
+
+open Models
+open Xunit
+open FsUnit
+open FsUnit.Xunit
+open FsCheck
+open FsCheck.Xunit
+
+module SeqTests =
+
+    [<Property>]
+    let ``takeAtMost returns min of seq size and take size`` (NonNegativeInt sourceSize) (NonNegativeInt takeSize) =
+        let sourceSize = sourceSize % 5
+        let takeSize = takeSize % 10
+        let expectedResultSize = min sourceSize takeSize
+        let source = Seq.replicate sourceSize "a"
+
+        let actual =
+            source
+            |> Seq.takeAtMost expectedResultSize
+            |> List.ofSeq
+
+        let expected =
+            source
+            |> Seq.take expectedResultSize
+            |> List.ofSeq
+
+        actual |> should equal expected
+
+    [<Property>]
+    let ``takeAtMost throws if take size is less than 0`` (NegativeInt takeSize) (s:string) =
+        let takeNegative () = s |> Seq.takeAtMost takeSize |> ignore
+        takeNegative |> shouldFail
