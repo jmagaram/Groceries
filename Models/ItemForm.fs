eb83843e411cb5a53032efec2a7bcd722a4905bb
Justin Magaram <justin@magaram.com>
2020-10-09 16:46:05 -0700

    fix glitchy typing in textbox in item edit form - NEVER update the bound value while the user is typing (worked on windows sortof but failed bad on ios)

--- Models/ItemForm.fs
+++ Models/ItemForm.fs
@@ -13,15 +13,19 @@ type CategoryMode =
 
 type Form =
     { ItemId: StateTypes.ItemId option
+      ItemNameProposed: string
       ItemName: string
       Etag: StateTypes.Etag option
       Quantity: string
+      QuantityProposed: string
       Note: string
+      NoteProposed: string
       ScheduleKind: ScheduleKind
       Frequency: StateTypes.Frequency
       Postpone: int<days> option
       CategoryMode: CategoryMode
       NewCategoryName: string
+      NewCategoryNameProposed: string
       CategoryChoice: StateTypes.Category option
       CategoryChoiceList: StateTypes.Category list
       Stores: QueryTypes.ItemAvailability list }
@@ -54,15 +58,26 @@ type ItemFormMessage =
     | Purchased
     | Transaction of ItemFormMessage seq
 
-let itemNameValidation f = f.ItemName |> ItemName.tryParse
-let itemNameChange s f = { f with ItemName = s }
-let itemNameBlur f = { f with ItemName = f.ItemName |> ItemName.normalizer }
-let quantityValidation f = f.Quantity |> Quantity.tryParseOptional
-let quantityChange s f = { f with Quantity = s }
-let quantityBlur f = { f with Quantity = f.Quantity |> Quantity.normalizer }
-let noteChange s f = { f with Note = s }
-let noteValidation f = f.Note |> Note.tryParseOptional
-let noteBlur f = { f with Note = f.Note |> Note.normalizer }
+let itemNameValidation f = f.ItemNameProposed |> ItemName.tryParse
+let itemNameChange s f = { f with ItemNameProposed = s }
+
+let itemNameBlur f =
+    { f with
+          ItemName = f.ItemNameProposed |> ItemName.normalizer }
+
+let quantityValidation f = f.QuantityProposed |> Quantity.tryParseOptional
+let quantityChange s f = { f with QuantityProposed = s }
+
+let quantityBlur f =
+    { f with
+          Quantity = f.QuantityProposed |> Quantity.normalizer }
+
+let noteChange s f = { f with NoteProposed = s }
+let noteValidation f = f.NoteProposed |> Note.tryParseOptional
+
+let noteBlur f =
+    { f with
+          Note = f.NoteProposed |> Note.normalizer }
 
 let categoryModeChooseExisting f = { f with CategoryMode = ChooseExisting }
 let categoryModeCreateNew f = { f with CategoryMode = CreateNew }
@@ -80,12 +95,14 @@ let chooseCategory i f =
               |> List.find (fun j -> j.CategoryId = StateTypes.CategoryId i)
               |> Some }
 
-let categoryNameValidation f = f.NewCategoryName |> CategoryName.tryParse
+let categoryNameValidation f =
+    f.NewCategoryNameProposed |> CategoryName.tryParse
 
-let categoryNameChange s f = { f with NewCategoryName = s }
+let categoryNameChange s f = { f with NewCategoryNameProposed = s }
 
 let categoryNameBlur (f: Form) =
-    let normalized = f.NewCategoryName |> CategoryName.normalizer
+    let normalized =
+        f.NewCategoryNameProposed |> CategoryName.normalizer
 
     let exists =
         f.CategoryChoiceList
@@ -105,7 +122,9 @@ let scheduleOnce f = { f with ScheduleKind = Once }
 let scheduleCompleted f = { f with ScheduleKind = Completed }
 let scheduleRepeat f = { f with ScheduleKind = Repeat }
 
-let frequencyCoerceIntoBounds d = Frequency.rules |> RangeValidation.forceIntoBounds d
+let frequencyCoerceIntoBounds d =
+    Frequency.rules
+    |> RangeValidation.forceIntoBounds d
 
 let frequencySet v f =
     { f with
@@ -141,7 +160,11 @@ let frequencyAsText (d: StateTypes.Frequency) =
     |> Option.defaultWith (fun () -> if d = 1 then "Daily" else sprintf "Every %i days" d)
 
 let postponeSet v f = { f with Postpone = Some v }
-let postponeUntilFrequency f = { f with Postpone = f.Frequency |> Frequency.days |> Some }
+
+let postponeUntilFrequency f =
+    { f with
+          Postpone = f.Frequency |> Frequency.days |> Some }
+
 let postponeClear f = { f with Postpone = None }
 let postponeDefault = None
 
@@ -153,8 +176,12 @@ let purchased f =
 
 let postponeDurationAsText (d: int<days>) =
     let d = d |> int
-    let monthsExactly = if d / 30 > 0 && d % 30 = 0 then Some(d / 30) else None
-    let weeksExactly = if d / 7 > 0 && d % 7 = 0 then Some(d / 7) else None
+
+    let monthsExactly =
+        if d / 30 > 0 && d % 30 = 0 then Some(d / 30) else None
+
+    let weeksExactly =
+        if d / 7 > 0 && d % 7 = 0 then Some(d / 7) else None
 
     match monthsExactly with
     | Some m -> if m = 1 then "1 month" else sprintf "%i months" m
@@ -164,7 +191,8 @@ let postponeDurationAsText (d: int<days>) =
         | None -> if d = 1 then "1 day" else sprintf "%i days" d
 
 let postponeChoices (f: Form) =
-    f.Postpone :: (Repeat.commonPostponeDays |> List.map Some)
+    f.Postpone
+    :: (Repeat.commonPostponeDays |> List.map Some)
     |> Seq.choose id
     |> Seq.map frequencyCoerceIntoBounds
     |> Seq.distinct
@@ -182,14 +210,18 @@ let canDelete (f: Form) = f.ItemId.IsSome
 let createNewItem itemName stores cats =
     { ItemId = None
       ItemName = itemName
+      ItemNameProposed = itemName
       Etag = None
       Quantity = ""
+      QuantityProposed = ""
       Note = ""
+      NoteProposed = ""
       ScheduleKind = ScheduleKind.Once
       Frequency = Frequency.goodDefault
       Postpone = postponeDefault
       CategoryMode = CategoryMode.ChooseExisting
       NewCategoryName = ""
+      NewCategoryNameProposed = ""
       CategoryChoice = None
       CategoryChoiceList =
           cats
@@ -206,12 +238,24 @@ let createNewItem itemName stores cats =
 let editItem (clock: Clock) cats (i: QueryTypes.ItemQry) =
     { ItemId = Some i.ItemId
       ItemName = i.ItemName |> ItemName.asText
+      ItemNameProposed = i.ItemName |> ItemName.asText
       Etag = i.Etag
       Quantity =
           i.Quantity
           |> Option.map Quantity.asText
           |> Option.defaultValue ""
-      Note = i.Note |> Option.map Note.asText |> Option.defaultValue ""
+      QuantityProposed =
+          i.Quantity
+          |> Option.map Quantity.asText
+          |> Option.defaultValue ""
+      Note =
+          i.Note
+          |> Option.map Note.asText
+          |> Option.defaultValue ""
+      NoteProposed =
+          i.Note
+          |> Option.map Note.asText
+          |> Option.defaultValue ""
       ScheduleKind =
           match i.Schedule with
           | StateTypes.Schedule.Completed -> Completed
@@ -229,6 +273,7 @@ let editItem (clock: Clock) cats (i: QueryTypes.ItemQry) =
           | StateTypes.Schedule.Repeat r -> r |> Repeat.due (clock ())
       CategoryMode = CategoryMode.ChooseExisting
       NewCategoryName = ""
+      NewCategoryNameProposed = ""
       CategoryChoice = i.Category
       CategoryChoiceList =
           cats
@@ -307,7 +352,9 @@ let asItemFormResult (now: DateTimeOffset) (f: Form) =
               | Once -> StateTypes.Schedule.Once
               | Repeat ->
                   { StateTypes.Repeat.Frequency = f.Frequency
-                    StateTypes.Repeat.PostponedUntil = f.Postpone |> Option.map (fun d -> now.AddDays(d |> float)) }
+                    StateTypes.Repeat.PostponedUntil =
+                        f.Postpone
+                        |> Option.map (fun d -> now.AddDays(d |> float)) }
                   |> StateTypes.Repeat }
 
     let notSold =
@@ -318,29 +365,6 @@ let asItemFormResult (now: DateTimeOffset) (f: Form) =
     { Item = item
       InsertCategory = insertCategory
       NotSold = notSold }
-
-// Delete and CanDelete
-// Submit (if no errors)
-//
-
-//Add to shopping list again
-//ONLY IF Completed
-
-//Mark complete and schedule next and submit
-//ONLY IF Repeating AND CanSubmit
-
-//Postpone x days (without commit)
-//ONLY if postponed
-
-//Remove recurrence and add to shopping list now (without commit)
-//ONLY IF repeat and postponed
-
-//Postpone x days (with commit)
-//ONLY IF repeating and not postponed
-
-//Mark complete and done
-//ONLY IF can submit and Once
-
 
 type Form with
     member me.ItemNameValidation = me |> itemNameValidation
