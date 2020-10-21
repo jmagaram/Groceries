9da27aa3219bfab6e32355104997cdecffd321e5
Justin Magaram <justin@magaram.com>
2020-10-21 10:00:41 -0700

    Do method on Option extension

--- WebApp/Common/FSharpExtensionMethods.cs
+++ WebApp/Common/FSharpExtensionMethods.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.FSharp.Core;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.FSharp.Core;
 
 namespace WebApp.Common {
     public static class OptionExtensions {
@@ -9,6 +10,12 @@ namespace WebApp.Common {
         public static bool IsSomeValue<T>(this FSharpOption<T> o, T value) => o.IsSome() && o.Value.Equals(value);
 
         public static bool IsNone<T>(this FSharpOption<T> o) => FSharpOption<T>.get_IsNone(o);
+
+        public static void Do<T>(this FSharpOption<T> o, Action<T> action) {
+            if (o.IsSome()) {
+                action(o.Value);
+            }
+        }
 
         public static IEnumerable<T> AsEnumerable<T>(this FSharpOption<T> o) =>
             o.IsSome() ? Enumerable.Repeat(o.Value, 1) : Enumerable.Empty<T>();
