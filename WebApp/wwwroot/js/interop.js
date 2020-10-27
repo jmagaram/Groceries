af890737176d7a50a0a0facb12fee13784cbdee4
Justin Magaram <justin@magaram.com>
2020-10-27 11:30:36 -0700

    rename Elix event to "openChanged", the official name

--- WebApp/wwwroot/js/interop.js
+++ WebApp/wwwroot/js/interop.js
@@ -1,8 +1,8 @@
 ﻿var ElixMenuButton = ElixMenuButton || {};
 
-// Attaches an event listener to the "close" event on an elix-menu-button
-// element. When the user selects an item from the menu, the supplied method is
-// called with the tag of the selected item.
+// Attaches an event listener to the "close" event on an elix-menu-button. When
+// the user selects an item from the menu, the supplied method is called with
+// the tag of the selected item.
 ElixMenuButton.addCloseEventListener = function (element, assemblyName, method, helper) {
     element.addEventListener("close", (event) => {
         const closeResult = event.detail.closeResult;
@@ -12,7 +12,11 @@ ElixMenuButton.addCloseEventListener = function (element, assemblyName, method, 
     })
 };
 
-ElixMenuButton.addOpenChangedEventListener = function (element, assemblyName, method, helper) {
+// Attaches an event listener to the openedchange event on an elix-menu-button.
+// When the menu is opened or closed, the supplied method is called indicating
+// whether the menu is opened or closed.
+ElixMenuButton.addOpenedChangeEventListener = function (element, assemblyName, method, helper) {
+    // Occasionally the method is called on a null element. I don't know why.
     if (element == null)
         return;
     element.addEventListener("openedchange", (event) => {
