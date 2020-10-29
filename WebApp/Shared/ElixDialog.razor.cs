50aad6ecd58b12574d83aa09b7568d6eafc9a7e8
Justin Magaram <justin@magaram.com>
2020-10-28 20:25:50 -0700

    code behind for ElixDialog

---
+++ WebApp/Shared/ElixDialog.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace WebApp.Shared {
+    public partial class ElixDialog {
+
+        bool _isOpen;
+        bool _isOpenChanged = false;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            set
+            {
+                if (_isOpen != value) {
+                    _isOpen = value;
+                    _isOpenChanged = true;
+                    StateHasChanged();
+                }
+            }
+        }
+
+        [Parameter]
+        public EventCallback<bool> IsOpenChanged { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender) {
+            if (_isOpenChanged) {
+                _isOpenChanged = false;
+                await IsOpenChanged.InvokeAsync(IsOpen);
+            }
+        }
+    }
+}
