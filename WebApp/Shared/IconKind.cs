a213c6aee354482e8160443e842b76fb33209107
Justin Magaram <justin@magaram.com>
2020-10-31 23:11:24 -0700

    add icon component

---
+++ WebApp/Shared/IconKind.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Shared {
+    public enum IconKind {
+        Shop,
+        Repeat,
+        CloudUpload,
+        Calendar,
+        Trash,
+        Check
+    }
+}
