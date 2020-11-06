82fc6a620abcc02825a13a3e605b860061947105
Justin Magaram <justin@magaram.com>
2020-11-05 17:19:29 -0800

    forgot Elix Option so dropdownlist items have a check mark

--- WebApp/scripts/index.js
+++ WebApp/scripts/index.js
@@ -7,4 +7,6 @@ import PopupButton from '../node_modules/elix/define/PopupButton.js';
 import Popup from '../node_modules/elix/define/Popup.js';
 import Dialog from '../node_modules/elix/define/Dialog.js';
 import DropdownList from "../node_modules/elix/define/DropdownList.js";
+import Option from "../node_modules/elix/define/Option.js";
+
 
