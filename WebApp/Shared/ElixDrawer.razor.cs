266cac1075b310a0f2c6cad9b434579df45c6176
Justin Magaram <justin@magaram.com>
2020-11-23 13:21:28 -0800

    add ElixDrawer wrapper

--- WebApp/Shared/ElixDrawer.razor.cs
+++ WebApp/Shared/ElixDrawer.razor.cs
@@ -1,10 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using WebApp.Common;
 
 namespace WebApp.Shared {
-    public partial class ElixDrawer {
+    public partial class ElixDrawer : IDisposable {
         ElementReference _drawer;
+        DotNetObjectReference<CallbackHelper<bool>> _openedChangeCallbackReference;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender) {
+            if (firstRender) {
+                var _openedChangeCallback = new CallbackHelper<bool>(OnOpenedChange);
+                _openedChangeCallbackReference = DotNetObjectReference.Create(_openedChangeCallback);
+                await JSRuntime.InvokeVoidAsync("ElixOpenCloseMixin.addOpenedChangeEventListener",
+                    _drawer,
+                    "WebApp",
+                    "Invoke",
+                    _openedChangeCallbackReference);
+            }
+        }
+
+        protected Task OnOpenedChange(bool isOpen) {
+            if (!isOpen) {
+                return Closed.InvokeAsync();
+            }
+            else {
+                return Task.CompletedTask;
+            }
+        }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
@@ -15,10 +39,29 @@ namespace WebApp.Shared {
         [Parameter]
         public ElixDrawerEdge FromEdge { get; set; } = ElixDrawerEdge.Start;
 
+        /// <summary>
+        /// Raised when the Drawer is closed by (1) clicking on the overlay, (2)
+        /// pressing ESC, or (3) dragging the drawer closed. Not raised as a
+        /// result of code that calls the Close() method.
+        /// </summary>
+        /// <remarks>
+        /// Can't get reliably notified of Open and Close events. The only
+        /// notification that works is getting a Close notification if the user
+        /// clicks outside the Drawer on the overlay or presses ESC or drags it
+        /// closed. No other events are raised properly.
+        /// </remarks>
+        [Parameter]
+        public EventCallback Closed { get; set; }
+
         public async ValueTask Open() =>
             await JSRuntime.InvokeVoidAsync("HtmlElement.setProperty", _drawer, "opened", true);
 
         public async ValueTask Close() =>
             await JSRuntime.InvokeVoidAsync("HtmlElement.setProperty", _drawer, "opened", false);
+
+        public void Dispose() {
+            _openedChangeCallbackReference?.Dispose();
+            _openedChangeCallbackReference = null;
+        }
     }
 }
