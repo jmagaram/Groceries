5103b688e6a8d26674a8711cfdb8a7229a27d993
Justin Magaram <justin@magaram.com>
2020-12-20 11:43:46 -0800

    add FilterComboBox using webpack

--- WebApp/scripts/index.js
+++ WebApp/scripts/index.js
@@ -9,4 +9,4 @@ import Dialog from '../node_modules/elix/define/Dialog.js';
 import DropdownList from "../node_modules/elix/define/DropdownList.js";
 import Option from "../node_modules/elix/define/Option.js";
 import Drawer from 'elix/define/Drawer.js';
-
+import FilterComboBox from 'elix/define/FilterComboBox.js';
