dfdd8371bda06bb02b373c3ab8b6632be5c76376
Justin Magaram <justin@magaram.com>
2020-12-21 16:39:45 -0800

    simplify elixdialog wrapper; the event binding wasn't working, and haven't used it yet

--- WebApp/Shared/ElixDialog.razor.cs
+++ WebApp/Shared/ElixDialog.razor.cs
@@ -1,11 +1,8 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace WebApp.Shared {
     public partial class ElixDialog {
-
         bool _isOpen;
-        bool _isOpenChanged = false;
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
@@ -18,19 +15,7 @@ namespace WebApp.Shared {
             {
                 if (_isOpen != value) {
                     _isOpen = value;
-                    _isOpenChanged = true;
-                    StateHasChanged();
                 }
-            }
-        }
-
-        [Parameter]
-        public EventCallback<bool> IsOpenChanged { get; set; }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender) {
-            if (_isOpenChanged) {
-                _isOpenChanged = false;
-                await IsOpenChanged.InvokeAsync(IsOpen);
             }
         }
     }
