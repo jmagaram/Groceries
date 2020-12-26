4ebfee723ebe8be5829c74a2b839a532071b36a6
Justin Magaram <justin@magaram.com>
2020-12-26 14:50:11 -0800

    animation postpone badge when changed

--- WebApp/wwwroot/js/general.js
+++ WebApp/wwwroot/js/general.js
@@ -5,3 +5,19 @@
 export function getProperty(element, key) {
     return element[key];
 }
+
+export function startAnimation(element, animationClass, timeout = 1000) {
+    element.classList.remove(animationClass);
+    void element.offsetWidth; // https://bit.ly/3pilQim
+    const stopAnimation = function () {
+        if (element.classList.contains(animationClass)) {
+            element.classList.remove(animationClass);
+            element.removeEventListener("animationend", stopAnimation);
+            element.removeEventListener("animationcancel", stopAnimation);
+        }
+    }
+    element.addEventListener("animationend", stopAnimation);
+    element.addEventListener("animationcancel", stopAnimation);
+    element.classList.add(animationClass);
+    setTimeout(stopAnimation, timeout);
+}
