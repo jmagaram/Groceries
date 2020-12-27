dbdb23389c75e179a04eb2a954badd9b0c8e3b08
Justin Magaram <justin@magaram.com>
2020-12-26 17:24:13 -0800

    fix animation getting truncated due to component rendering twice

--- WebApp/wwwroot/js/general.js
+++ WebApp/wwwroot/js/general.js
@@ -7,8 +7,15 @@ export function getProperty(element, key) {
 }
 
 export function startAnimation(element, animationClass, timeout = 1000) {
-    element.classList.remove(animationClass);
+
+    // Blazor in server mode sometimes renders a component instance twice.
+    // This causes animations to get visibly interrupted, though not when
+    // running on localhost. This check below - to see if an animation is 
+    // already in progress - seems to fix the issue.
+    if (element.classList.contains(animationClass)) return;
+
     void element.offsetWidth; // https://bit.ly/3pilQim
+
     const stopAnimation = function () {
         if (element.classList.contains(animationClass)) {
             element.classList.remove(animationClass);
@@ -16,6 +23,7 @@ export function startAnimation(element, animationClass, timeout = 1000) {
             element.removeEventListener("animationcancel", stopAnimation);
         }
     }
+
     element.addEventListener("animationend", stopAnimation);
     element.addEventListener("animationcancel", stopAnimation);
     element.classList.add(animationClass);
