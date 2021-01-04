cfc59299adefcdb7cff3a00923d113b96cc94665
Justin Magaram <justin@magaram.com>
2021-01-04 10:56:59 -0800

    remove old WASM app - no user authentication included

---
+++ GroceryApp/Client/Common/EnumerableExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace GroceryApp.Common
+{
+    public static class EnumerableExtensionMethods
+    {
+        public static T FirstOr<T>(this IEnumerable<T> items, T otherwise)
+        {
+            var (item, exists) = items.Select(i => (i, true)).FirstOrDefault();
+            return exists ? item : otherwise;
+        }
+    }
+}
---
+++ GroceryApp/Client/Common/TimeSpanEstimateExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using static Models.ViewTypes;
+
+#nullable enable
+namespace GroceryApp.Common
+{
+    public static class TimeSpanEstimateExtensions
+    {
+        public static TimeSpan AsTimeSpan(this TimeSpanEstimate e) => TimeSpanEstimateModule.toTimeSpan(e);
+
+        public static bool IsLessThanOrEqualTo(this TimeSpanEstimate e, TimeSpan t) => e.AsTimeSpan() <= t;
+
+        public static bool IsZeroOrNegative(this TimeSpanEstimate e) => e.IsLessThanOrEqualTo(TimeSpan.Zero);
+
+        public static string Format(
+            this TimeSpanEstimate e,
+            Func<int, string> days,
+            Func<int, string> weeks,
+            Func<int, string> months)
+        {
+            if (e is TimeSpanEstimate.Days d)
+            {
+                return days(d.Item);
+            }
+            else if (e is TimeSpanEstimate.Weeks w)
+            {
+                return weeks(w.Item);
+            }
+            else if (e is TimeSpanEstimate.Months m)
+            {
+                return months(m.Item);
+            }
+            else throw new NotImplementedException();
+        }
+
+        private static string JoinQuantityUnits(int n, string singular, string plural, string separator)
+        {
+            string units = ((n == 1) || (n == -1)) ? singular : plural;
+            return $"{n}{separator}{units}";
+        }
+
+        public static string Format(
+            this TimeSpanEstimate e,
+            string separator,
+            string days1,
+            string daysN,
+            string weeks1,
+            string weeksN,
+            string months1,
+            string monthsN) => e.Format(i => JoinQuantityUnits(i, days1, daysN, separator),
+                                        i => JoinQuantityUnits(i, weeks1, weeksN, separator),
+                                        i => JoinQuantityUnits(i, months1, monthsN, separator));
+    }
+}
